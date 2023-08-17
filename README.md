51fff13a1dc2a33cb2e302abec927718b582bec7
Arturo Herrera <arti.herrera@gmail.com>
2023-08-16 18:25:24 -0600

    Se añadieron instrucciones al README

---
+++ README.md
@@ -0,0 +1,42 @@
+# Análisis de Emociones en Videos
+
+Este proyecto utiliza DeepFace para analizar las emociones en un video. El script procesa un video y guarda los resultados en un archivo CSV, incluyendo información como el número de fotograma, los segundos en el tiempo, y las emociones detectadas.
+
+## Requisitos
+
+- Python 3.7 o superior
+- Bibliotecas: \`tkinter\`, \`cv2\`, \`pandas\`, \`DeepFace\`
+
+Puedes instalar las bibliotecas requeridas utilizando el siguiente comando:
+
+\`\`\`bash
+pip install opencv-python pandas deepface
+\`\`\`
+
+## Cómo Ejecutar
+
+1. **Clonar el Repositorio:** Clona este repositorio en tu máquina local utilizando \`git clone <URL_DEL_REPOSITORIO>\`.
+
+2. **Navegar al Directorio:** Navega al directorio donde se encuentra el script en la terminal.
+
+3. **Ejecutar el Script:** Ejecuta el siguiente comando para iniciar el script:
+
+   \`\`\`bash
+   python nombre_del_script.py
+   \`\`\`
+
+4. **Seleccionar el Video:** Se abrirá una ventana para que selecciones el video que deseas analizar.
+
+5. **Guardar los Resultados:** Se abrirá otra ventana para que selecciones dónde guardar el archivo CSV con los resultados.
+
+6. **Revisar los Resultados:** Una vez que el script haya terminado de ejecutar, encontrarás el archivo CSV en la ubicación seleccionada.
+
+## Información Adicional
+
+- **ID del Usuario y del Video:** El script te pedirá que ingreses un ID de usuario y un ID de video que se guardarán junto con los resultados.
+
+- **Emociones Analizadas:** Las emociones analizadas incluyen felicidad, tristeza, sorpresa, miedo, disgusto, ira y neutralidad.
+
+## Soporte
+
+Si tienes alguna pregunta o encuentras algún problema, por favor crea un Issue en este repositorio.
--- main.py
+++ main.py
@@ -1,32 +1,55 @@
-def process_video(video_path, output_path):
-    # Leer el video
+import tkinter as tk
+from tkinter import filedialog
+from deepface import DeepFace
+import cv2
+import pandas as pd
+
+def process_video(video_path, output_path, id_usuario, id_video):
+    print("Procesando video:", video_path)
+
     cap = cv2.VideoCapture(video_path)
     fps = int(cap.get(cv2.CAP_PROP_FPS))
-    
-    # Estructura para almacenar las emociones
-    emotions = []
 
+    emotions = []
     frame_number = 0
+
     while cap.isOpened():
         ret, frame = cap.read()
         if ret:
-            # Procesar la imagen con DeepFace
             result = DeepFace.analyze(frame, actions=['emotion'])
-            
-            # Calcular el tiempo del frame actual
             time_seconds = frame_number / fps
-
-            # Agregar las emociones al registro junto con el número de frame y el tiempo
-            emotion_record = {'frame_number': frame_number, 'time_seconds': time_seconds, **result['emotion']}
+            emotion_data = result[0]['emotion']
+            emotion_record = {'id_usuario': id_usuario, 'id_video': id_video, 'frame_number': frame_number, 'time_seconds': time_seconds, **emotion_data}
             emotions.append(emotion_record)
-
             frame_number += 1
         else:
             break
 
-    # Crear un DataFrame con las emociones y guardarlo como CSV
     emotions_df = pd.DataFrame(emotions)
     emotions_df.to_csv(output_path, index=False)
 
     cap.release()
     cv2.destroyAllWindows()
+
+    print("Procesamiento completo. Resultados guardados en:", output_path)
+
+def select_and_process_video():
+    print("Iniciando la selección de video...")
+
+    root = tk.Tk()
+    root.withdraw()
+    video_path = filedialog.askopenfilename(title="Selecciona el video")
+    print("Video seleccionado:", video_path)
+
+    output_path = filedialog.asksaveasfilename(title="Guardar como", defaultextension=".csv", filetypes=[("Archivos CSV", "*.csv")])
+
+    id_usuario = input("Por favor, ingrese el ID de usuario que desea asignar: ") # Pide al usuario el ID de usuario
+    id_video = input("Por favor, ingrese el ID de video que desea asignar: ") # Pide al usuario el ID de video
+
+    if video_path and output_path:
+        process_video(video_path, output_path, id_usuario, id_video)
+    else:
+        print("Operación cancelada.")
+
+if __name__ == "__main__":
+    select_and_process_video()
